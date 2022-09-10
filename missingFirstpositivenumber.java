87c2cb517363ef663bd1495ed71e2adbb158df85
VIKASH KUMAR <champvikash4@gmail.com>
2022-09-10 14:57:41 +0530

    Create missingFirstpositivenumber.java

---
+++ missingFirstpositivenumber.java
@@ -0,0 +1,33 @@
+package com.example.mycode;
+
+public class missingFirstpositivenumber {
+
+    public static int missingNumber(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i]-1;
+            if (arr[i]>0 && arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        // search for first missing number
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index) {
+                return index + 1;
+            }
+        }
+
+        // case 2
+        return arr.length+1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
+}
