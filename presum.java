fdf99361d803e3760b4e8cd19f0cd4e46976fae5
VIKASH KUMAR <champvikash4@gmail.com>
2022-09-19 23:01:26 +0530

    Create presum.java

---
+++ presum.java
@@ -0,0 +1,36 @@
+package com.example.mycode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class presum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n], sumpre=new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+
+
+        int l=sc.nextInt() , r=sc.nextInt();
+
+
+
+        for(int i=0; i<n; i++){
+           if(i==0){
+               sumpre[i]=arr[i];
+           }else{
+               sumpre[i] += arr[i-1] +arr[i];
+           }
+        }
+
+        for(int i=l; i<r; i++){
+            System.out.println(Arrays.toString(sumpre));
+        }
+
+
+
+    }
+}
