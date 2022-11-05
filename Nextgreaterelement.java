ea4b9381622b44674f3cf93045deb1d0e414d1bb
VIKASH KUMAR <champvikash4@gmail.com>
2022-11-06 01:20:58 +0530

    Update Nextgreaterelement.java

--- Nextgreaterelement.java
+++ Nextgreaterelement.java
@@ -1,3 +1,6 @@
+//https://practice.geeksforgeeks.org/problems/next-larger-element-1587115620/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+
 package com.example.mycode.stack;
 
 import java.util.Arrays;
