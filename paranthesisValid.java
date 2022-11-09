549b332b87cf5bdc02f18ea06339b3376d51981a
VIKASH KUMAR <champvikash4@gmail.com>
2022-11-10 03:12:20 +0530

    Create paranthesisValid.java

---
+++ paranthesisValid.java
@@ -0,0 +1,31 @@
+package com.example.mycode.stack;
+
+import java.util.Stack;
+
+public class paranthesisValid {
+    public static void main(String[] args) {
+        String str = "{([])}";
+        System.out.println(isValid(str));
+    }
+
+    public static boolean isValid(String x){
+        // add your code here
+
+        Stack<Character> stk = new Stack<>();
+
+        for(char ch : x.toCharArray() ) {
+            if (ch == '(') {
+                stk.push(')');
+            } else if (ch == '{') {
+                stk.push('}');
+            } else if (ch == '[') {
+                stk.push(']');
+            } else if (!stk.isEmpty() && ch == stk.peek()) {
+                stk.pop();
+            } else {
+                return false;
+            }
+        }
+        return stk.isEmpty();
+    }
+}
