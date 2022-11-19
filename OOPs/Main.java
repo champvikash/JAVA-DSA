85b2d026c6c7040267858a4d6a206e80153a95a3
VIKASH KUMAR <champvikash4@gmail.com>
2022-11-20 02:44:23 +0530

    Create Main.java

---
+++ OOPs/Main.java
@@ -0,0 +1,30 @@
+package com.example.mycode.OOPs;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static class YouTubevideo {
+        String name;
+        int numberofViews, numberofLikes;
+        ArrayList<String> comments;
+    }
+
+    public static void main(String[] args) {
+        YouTubevideo songVideo = new YouTubevideo();
+        songVideo.name = "Keshariya";
+        songVideo.numberofLikes = 1000000;
+        songVideo.numberofViews = 1000001;
+        songVideo.comments = new ArrayList<>();
+        songVideo.comments.add("good");
+        songVideo.comments.add("nice");
+        System.out.println("Name of video:" + songVideo.name);
+        System.out.println("numberoflikes:" + songVideo.numberofLikes);
+        System.out.println("numberofViews:" + songVideo.numberofViews);
+        System.out.println("comments:" + songVideo.comments);
+
+
+
+    }
+
+}
