e70152cd7de4f5101731e9c2c73571348f28f89c
VIKASH KUMAR <champvikash4@gmail.com>
2023-01-06 02:35:13 +0530

    matrixquestion

---
+++ matrix/arrayquestion.java
@@ -0,0 +1,31 @@
+package com.example.mycode.matrix;
+
+import java.util.Stack;
+
+public class arrayquestion {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 2, 4, 5};
+
+        Stack<Integer> stk = new Stack<>();
+
+        for (int i=0; i<arr.length; i++){
+            stk.push(arr[i]);
+        }
+
+        System.out.println(stk);
+
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        for (int i=0; i<arr.length; i++){
+            stk.pop();
+        }
+
+        System.out.println(stk);
+
+
+    }
+}
